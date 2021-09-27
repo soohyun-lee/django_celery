77363d003b3845cdfe85bf8e1441191ffe3a1d59
viva-lydia <lydia@adoc.co.kr>
2021-09-27 22:50:58 +0900

    Delete ilnass directory

--- ilnass/ilnass/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('products', include('product.urls')),
-]
-
-
--- ilnass/product/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views      import Allproducts, RecommendView, DetailView, Basicinformation
-urlpatterns = [
-    path('',Allproducts.as_view()),
-    path('/recommend',RecommendView.as_view()),
-    path('/detail',DetailView.as_view()),
-    path('/creator',Basicinformation.as_view()),
-]
